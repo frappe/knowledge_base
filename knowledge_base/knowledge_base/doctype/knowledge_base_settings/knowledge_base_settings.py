321e540d05ac809b25270e0fc6411f5e8f4e5bc8
Rushabh Mehta <rmehta@gmail.com>
2014-11-12 13:05:33 +0530

    cache

--- knowledge_base/knowledge_base/doctype/knowledge_base_settings/knowledge_base_settings.py
+++ knowledge_base/knowledge_base/doctype/knowledge_base_settings/knowledge_base_settings.py
@@ -4,8 +4,8 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from frappe.website.utils import delete_page_cache
+from knowledge_base.utils import clear_cache
 
 class KnowledgeBaseSettings(Document):
 	def on_update(self):
-		delete_page_cache("kb")
+		clear_cache()
